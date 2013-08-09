25089a547d605e8e6c8c50c850b69fcba26c179f
Gary Graham <garygrantgraham@gmail.com>
2013-08-09 13:44:05 -0400

    Committing backup script
    Now moves files and folders safely, creates log files.

--- backupScript/backupScript.py
+++ backupScript/backupScript.py
@@ -2,26 +2,28 @@ import os
 import time
 import shutil
 import sys
-import logging
+import logger
+
 def get_folder_size(path):
     totSize = 0
-    for root, dirs, files in os.walk(path):
+    for root, subdirectories, files in os.walk(path):
             for name in files:
                 totSize += round(os.path.getsize(os.path.join(root, name)) / 1048576)
     return totSize
 
+def get_top_level_files_in_path(path):
+    file_list = []
+    for file_or_folder in os.listdir(path):
+        file_path = os.path.join(path, file_or_folder)
+        mod_time = (os.stat(file_path)).st_mtime
+        file_list.append([file_path, mod_time])
 
-def get_all_files_in_path(path):
-    fileList = []
-    for root, dirs, files in os.walk(path):
-        for name in files:
-            infoList = []
-            filePath = os.path.join(root, name)
-            infoList.append(filePath)
-            stat = os.stat(filePath)
-            infoList.append(stat.st_mtime)
-            fileList.append(infoList)
-    return fileList
+    print("starting list dump")
+    for item in file_list:
+        print(item)
+    print("ending list dump")
+
+    return file_list
 
 
 def grab_oldest_half_of_files_in_list(fileList):
@@ -31,43 +33,42 @@ def grab_oldest_half_of_files_in_list(fileList):
     return fileList
 
 
-def move_files_to_backup(fileList, backupPath):
-    for current_file in fileList:
+def move_files_and_folders(file_or_folder_list, destination_path):
+    for file_or_folder in file_or_folder_list:
         try:
-            shutil.move(current_file[0].strip(), os.path.join(backupPath))
-            logger.info(" ----> Moved : " + current_file[0])
-        except (IOError, OSError) as why:
-            logger.error(" ---->Failed to copy " + current_file[0] +
-                ". Error:" + str(why))
+            shutil.move(file_or_folder[0], destination_path)
+            logger.info(" ----> " + file_or_folder[0] + "\t" "---->" + "\t" + destination_path)
+        except(IOError, OSError, shutil.Error) as error:
+            logger.error(" ----> Failed to copy " + file_or_folder[0] +
+                ". Error:" + str(error))
 
-if __name__ == '__main__':
-    #logging setup
-    logger = logging.getLogger('backupScript')
-    logger.setLevel(logging.INFO)
-    hdlr = logging.FileHandler(os.path.join(os.path.dirname(os.path.realpath(__file__)), "backupScript.log"))
-    formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
-    hdlr.setFormatter(formatter)
-    logger.addHandler(hdlr)
 
+def parse_arguments():
     print(sys.argv)
     if len(sys.argv) == 4:
         sourcePath = sys.argv[1]
         destPath = sys.argv[2]
-        if  not destPath.endswith('\\'):
-            destPath += '\\'
+        threshold_in_mb = int(sys.argv[3])
+
+        #if not destPath.endswith('\\'):
+         #   destPath += '\\'
         if not os.path.exists(destPath):
             os.mkdir(destPath)
             logger.info(" ----> Could not find destination folder. Creating " + destPath)
     else:
         print('''The usage of backupScript.py is the following\n
-         python backupScript.py sourceDir DestDir folderSizeThresholdinGB\n
-         Example: python backupScript.py C:\\files D:\\backup 150''')
+         python backupScript.py sourceDir DestDir FolderSizeThreshold\n
+         Example: python backupScript.py C:\\files D:\\backup 1000''')
         sys.exit()
+    return sourcePath, destPath, threshold_in_mb
 
-
-    mb_size_threshold = sys.argv[3]
-    folSizeInMb = get_folder_size(sourcePath)
-    if(folSizeInMb > 1000):
-        listOfFiles = get_all_files_in_path(sourcePath)
+def initiate_half_backup(sourecPath, destPath):
+        listOfFiles = get_top_level_files_in_path(sourcePath)
         oldestHalf = grab_oldest_half_of_files_in_list(listOfFiles)
-        move_files_to_backup(oldestHalf, destPath)
+        move_files_and_folders(oldestHalf, destPath)
+
+if __name__ == '__main__':
+    logger = logger.Logger().getLogger("test")
+    sourcePath, destPath, threshold_in_mb = parse_arguments()
+    if(get_folder_size(sourcePath) > threshold_in_mb):
+        initiate_half_backup(sourcePath, destPath)
